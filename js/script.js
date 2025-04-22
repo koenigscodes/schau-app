ba0ffd0f6ed8547a8bb94386683206f7ac1c2bce
FavourWuche <favouruez11@gmail.com>
2025-04-22 21:48:16 +0100

    js added and changes made to index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,84 @@
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+async function displayPopularMovies(params) {
+  const { results }= await fetchAPIData('movie/popular');
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+       movie.poster_path
+               ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#popular-movies').appendChild(div);    
+  });
+
+}
+
+//fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'f9c700ceb48bee9879661e693aa097b0';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+  const data = await response.json();
+
+  return data;
+}
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');  
+    }
+  });
+}
+
+//init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':  
+      displayPopularMovies();
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie-details');    
+      break;
+      case '/tv-details.html':
+        console.log('Tv-details');    
+        break;
+        case '/search.html':
+          console.log('Search');    
+          break;    
+  }
+  highlightActiveLink(); 
+}
+
+document.addEventListener('DOMContentLoaded', init);
